5eb159d36a0a0c7c63e86faa95287968f91b314c
Fil8 <filippo.maccagni@gmail.com>
2018-03-11 06:29:23 +0100

    added main class

---
+++ radiobs.py
@@ -0,0 +1,8 @@
+__author__ = "Filippo Maccagni"
+__copyright__ = "Fil8"
+__email__ = "filippo.maccagni@gmail.com"
+
+from hi import hi
+from observe import observe
+from Cosmo import Cosmo
+from optical import optical

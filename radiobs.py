5de4764d178e867436201938d63243c7142e03d2
Fil8 <filippo.maccagni@gmail.com>
2018-04-04 12:34:59 +0200

    new modules

--- radiobs.py
+++ radiobs.py
@@ -2,7 +2,11 @@ __author__ = "Filippo Maccagni"
 __copyright__ = "Fil8"
 __email__ = "filippo.maccagni@gmail.com"
 
+from conv_units import conv_units
 from hi import hi
 from observe import observe
 from Cosmo import Cosmo
 from optical import optical
+from cubeful import cubeful
+from radcont import radcont
+#from absex import absex
